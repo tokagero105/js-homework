cb3a25d68fdf66104a48323f6e176675c3f540e7
Semyon Titov <tokagero105@yandex.ru>
2024-07-09 23:43:30 +0300

    fix 9-sort

--- 9-sort-loops/app.js
+++ 9-sort-loops/app.js
@@ -2,22 +2,24 @@ const arr = [1, 40, -5, 10, 0]
 
 // sortOrder - это параметр для указания порядка сортировки, где true - это по возрастанию, а false - это по убыванию
 function sort(array, sortOrder = true) {
-    let iteration_length = array.length
+    const newArray = array.slice()
+    let iteration_length = newArray.length
 
     while (iteration_length != 0) {
         for (let i = 1; i < iteration_length; i++) {
-            if ((array[i] < array[i - 1]) == sortOrder) {
-                let temp = array[i - 1]
-                array[i - 1] = array[i]
-                array[i] = temp
+            if ((newArray[i] < newArray[i - 1]) == sortOrder) {
+                let temp = newArray[i - 1]
+                newArray[i - 1] = newArray[i]
+                newArray[i] = temp
             }
         }
 
         iteration_length--
     }
 
-    return array
+    return newArray
 }
 
 console.log(arr)
 console.log(sort(arr, true))
+console.log(arr)
